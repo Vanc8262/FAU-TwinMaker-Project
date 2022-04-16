6e33424cc71fb32dcf6ae4db0b10d03b4f20f0e5
Vanc8262 <86838787+Vanc8262@users.noreply.github.com>
2022-04-15 21:22:48 -0400

    intial codebase
    Removed all code that was not needed. And modified to match needed struture

--- src/modules/sitewise/cdk/bin/sitewise.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import 'source-map-support/register';
--- src/modules/sitewise/cdk/lib/sitewise-stack.d.ts
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import * as cdk from '@aws-cdk/core';
-export declare class SiteWiseStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
